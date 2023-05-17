572c64444e821053cdad8d97176fe7107a59f304
Nik Kashi <mhmhkashi@gmail.com>
2023-05-17 10:45:55 +0200

    Implement states in different classes

--- src/main/scala/nl/vandebron/fsm/FsmApp.scala
+++ src/main/scala/nl/vandebron/fsm/FsmApp.scala
@@ -8,7 +8,12 @@ import zio.{ZIOAppDefault, _}
 object FsmApp extends ZIOAppDefault {
   def run = for {
     system <- ActorSystem("mySystem")
-    actor <- system.make("actor1", Supervisor.none, 0, stateful)
+    actor <- system.make(
+      "actor1",
+      Supervisor.none,
+      new OutOfStockState(),
+      stateful
+    )
     _ <- actor ! Feed(4)
     _ <- actor ! Vend()
     _ <- actor ! Vend()
--- src/main/scala/nl/vandebron/fsm/VendingFsm.scala
+++ src/main/scala/nl/vandebron/fsm/VendingFsm.scala
@@ -8,40 +8,50 @@ sealed trait Command[+_]
 case class Vend() extends Command[Int]
 case class Feed(stock: Int) extends Command[Int]
 
-object VendingFsm {
-  val stateful = new Stateful[Any, Int, Command] {
-    override def receive[A](
-        state: Int,
-        msg: Command[A],
-        context: Context
-    ): UIO[(Int, A)] =
-      msg match {
-        case Feed(value) =>
-          val newStock = state + value
-          println(s"Fed, new stock is $newStock")
-          ZIO.succeed((newStock, newStock))
-        case Vend() if state >= 1 =>
-          val newStock = state - 1
-          println(s"Vended, new stock is $newStock")
-          ZIO.succeed((newStock, newStock))
-        case Vend() =>
-          println("Failed to vend, out of stock")
-          ZIO.succeed((0, 0))
-      }
+sealed trait State {
+  val stock: Int
+  def receiver[A]: Command[A] => UIO[(State, A)]
+}
+
+class VendibleState(val stock: Int) extends State {
+  override def receiver[A]: Command[A] => UIO[(State, A)] = {
+    case Feed(value) =>
+      val newStock = stock + value
+      println(s"Fed, new stock is $newStock")
+      ZIO.succeed((new VendibleState(newStock), newStock))
+    case Vend() =>
+      val newStock = stock - 1
+      println(s"Vended, new stock is $newStock")
+      if (newStock > 0)
+        ZIO.succeed((new VendibleState(newStock), newStock))
+      else
+        ZIO.succeed((new OutOfStockState(), newStock))
+
   }
 }
 
-//sealed trait Command[+_]
-//case class DoubleCommand(value: Int) extends Command[Int]
-//object A {
-//  val stateful = new Stateful[Any, Unit, Command] {
-//    override def receive[A](
-//        state: Unit,
-//        msg: Command[A],
-//        context: Context
-//    ): UIO[(Unit, A)] =
-//      msg match {
-//        case DoubleCommand(value) => ZIO.succeed(((), value * 2))
-//      }
-//  }
-//}
+class OutOfStockState extends State {
+
+  override val stock: Int = 0
+  override def receiver[A]: Command[A] => UIO[(State, A)] = {
+    case Feed(value) =>
+      val newStock = stock + value
+      println(s"Fed, new stock is $newStock")
+      ZIO.succeed((new VendibleState(newStock), newStock))
+    case Vend() =>
+      println("Failed to vend, out of stock")
+      ZIO.succeed((new OutOfStockState(), 0))
+
+  }
+}
+object VendingFsm {
+  val stateful = new Stateful[Any, State, Command] {
+    override def receive[A](
+        state: State,
+        msg: Command[A],
+        context: Context
+    ): UIO[(State, A)] =
+      state.receiver[A](msg)
+
+  }
+}
